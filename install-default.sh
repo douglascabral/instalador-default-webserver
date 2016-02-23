792c910597b666eb0180cdc5055139a10c2f6534
Douglas Cabral <douglas.cabral@fiap.com.br>
2016-02-23 14:59:33 -0300

    Corrigido permissão para atualizar hosts

--- install-default.sh
+++ install-default.sh
@@ -112,7 +112,7 @@ echo "Reiniciando apache2"
 sudo service apache2 restart
 
 echo "Atualizando arquivo hosts"
-echo "127.0.1.1   test.local www.test.local" >> /etc/hosts
+sudo echo "127.0.1.1   test.local www.test.local" >> /etc/hosts
 
 echo -n "Pressione qualquer tecla para sair..."
 read
