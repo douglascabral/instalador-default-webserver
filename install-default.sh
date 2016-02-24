a05b926bdf9ad9f938d2216f5e9ff6fa4dea3e4e
Douglas Cabral <douglas.cabral@fiap.com.br>
2016-02-24 11:43:46 -0300

    Execução sudo do composer-setup

--- install-default.sh
+++ install-default.sh
@@ -122,7 +122,7 @@ echo "127.0.1.1   test.local www.test.local" | sudo tee --append /etc/hosts
 echo "Instalando o composer"
 php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
 php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === 'fd26ce67e3b237fffd5e5544b45b0d92c41a4afe3e3f778e942e43ce6be197b9cdc7c251dcde6e2a52297ea269370680') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); }"
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 php -r "unlink('composer-setup.php');"
 
 #Apaga o arquivo atual
