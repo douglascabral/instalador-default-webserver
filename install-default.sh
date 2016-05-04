097260c26aa6a46f3a905b7b7913a7338d9f4aa6
Douglas Cabral <douglas.cabral@fiap.com.br>
2016-05-04 17:18:14 -0300

    Atualizado pacotes para o PHP 7

--- install-default.sh
+++ install-default.sh
@@ -24,6 +24,7 @@ feedback()
 	echo 
 }
 
+# Verifica se há dependencias necessárias para rodar o script corretamente
 curl --version
 if ! [ $? -eq 0 ];
 then
@@ -32,7 +33,7 @@ then
 	sudo apt-get -y install curl
 fi
 
-#Repositório nodejs mais recente
+#Repositório nodejs LTS mais recente
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 
 # Atualiza a lista de pacotes
@@ -51,37 +52,54 @@ libsqlite3-dev
 libxml2-dev
 libxslt1-dev
 libffi-dev
-sqlite3
 git
+sqlite3
 apache2
 memcached
 mysql-server
 mysql-client
 libmysql++-dev
-php5
-libapache2-mod-php5
-php5-mysql
-php5-curl
-php5-gd
-php5-idn
+php7.0 
+libapache2-mod-php7.0
+php7.0-cgi
+php7.0-cli
+php7.0-common
+php7.0-curl
+php7.0-dev
+php7.0-gd
+php7.0-gmp
+php7.0-json
+php7.0-ldap
+php7.0-mysql
+php7.0-odbc
+php7.0-opcache
+php7.0-pgsql
+php7.0-pspell
+php7.0-readline
+php7.0-recode
+#php7.0-snmp
+php7.0-sqlite3
+php7.0-tidy
+php7.0-xml
+php7.0-xmlrpc
+libphp7.0-embed
+php7.0-bcmath
+php7.0-bz2
+php7.0-enchant
+php7.0-fpm
+php7.0-imap
+php7.0-interbase
+php7.0-intl
+php7.0-mbstring
+php7.0-mcrypt
+php7.0-phpdbg
+php7.0-soap
+php7.0-sybase
+php7.0-xsl
+php7.0-zip
 php-pear
-php5-imagick
-php5-imap
-php5-intl
-php5-mcrypt
-#php5-memcache
-php5-memcached
-php5-mhash
-php5-ming
-php5-ps
-php5-pspell
-php5-recode
-#php5-snmp
-php5-sqlite
-php5-tidy
-php5-xmlrpc
-php5-xsl
-php5-json
+php-imagick
+php-gettext
 nodejs
 npm
 )
